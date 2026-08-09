9f7432c7e2567838ec207276dadbd39cba14f112
blest <blest@shoujoyo>
2026-08-08 23:30:27 -0600

    Persona y Paciente listos!!

--- src/pkg_modelo/Paciente.java
+++ src/pkg_modelo/Paciente.java
@@ -8,6 +8,33 @@ package pkg_modelo;
  *
  * @author uli08
  */
-public class Paciente {
+public class Paciente extends Persona {
+    protected String alergias;
+    protected String tipoSangre;
+    
+    public Paciente(int i, String n, String a, int e, String t) {
+        super(i, n, a, e, t);
+    }
+    
+    @Override
+    public String toString() {
+        return nombre + " " + apellido + " " + alergias + " " + tipoSangre;
+    }
+
+    public String getAlergias() {
+        return alergias;
+    }
+
+    public void setAlergias(String alergias) {
+        this.alergias = alergias;
+    }
+
+    public String getTipoSangre() {
+        return tipoSangre;
+    }
+
+    public void setTipoSangre(String tipoSangre) {
+        this.tipoSangre = tipoSangre;
+    }
     
 }
--- src/pkg_modelo/Persona.java
+++ src/pkg_modelo/Persona.java
@@ -8,6 +8,59 @@ package pkg_modelo;
  *
  * @author uli08
  */
-public class Persona {
+public abstract class Persona {
+    protected int id;
+    protected String nombre;
+    protected String apellido;
+    protected int edad;
+    protected String telefono;
+
+    public Persona(int i, String n, String a, int e, String t) {
+        this.id = i;
+        this.nombre = n;
+        this.apellido = a;
+        this.edad = e;
+        this.telefono = t;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
     
 }
