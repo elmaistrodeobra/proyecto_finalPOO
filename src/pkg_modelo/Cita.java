80ff2e4c7bd0c7fb6a1395f630efab1089ddb39f
uli08 <uli08@uliECM>
2026-08-09 18:01:16 -0600

    clase Cita terminada

--- src/pkg_modelo/Cita.java
+++ src/pkg_modelo/Cita.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pkg_modelo;
 
-/**
- *
- * @author uli08
- */
 public class Cita {
-    
+    private int idCita;
+    private Paciente paciente;
+    private Medico medico;
+    private String fecha;
+    private String hora;
+    private String diagnostico;
+
+    public Cita(int idCita, Paciente paciente, Medico medico, String fecha, String hora, String diagnostico) {
+        this.idCita = idCita;
+        this.paciente = paciente;
+        this.medico = medico;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.diagnostico = diagnostico;
+    }
+
+    public Cita(Paciente paciente, Medico medico, String fecha, String hora, String diagnostico) {
+        this.paciente = paciente;
+        this.medico = medico;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.diagnostico = diagnostico;
+    }
+
+    public int getIdCita() 
+    { return idCita; }
+    public Paciente getPaciente() 
+    { return paciente; }
+    public Medico getMedico() 
+    { return medico; }
+    public String getFecha()
+    { return fecha; }
+    public String getHora() 
+    { return hora; }
+    public String getDiagnostico() 
+    { return diagnostico; }
 }
